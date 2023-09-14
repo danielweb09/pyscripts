1bad47efb6bede5e15d0978cab4a46a97e0a0898
danielweb09 <daniel.alvesb@gmail.com>
2023-09-14 18:06:22 -0300

    Add files via upload

---
+++ teste03 - data.py
@@ -0,0 +1,5 @@
+dia = input ('Qual o dia do seu nascimento? ')
+mes = input ('Qual o mes do seu nascimento? ')
+ano = input ('Qual o ano de seu nascimento? ')
+
+print('Olá, você nasceu em', dia, 'de', mes, 'de',  ano, 'correto?')
